02d3035c37b508e9deef5c4dcf79b5845af03479
Drazakk <deepanshu.3147@gmail.com>
2023-12-31 02:03:55 +0530

    Added Inputs and Top Buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      Hello World
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons/>
+      <Inputs/>
     </div>
   );
 }
