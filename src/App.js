00bf8d275ebb951fa70ad6e414e313d20ebc4838
Drazakk <deepanshu.3147@gmail.com>
2023-12-31 17:10:13 +0530

    Added Forecast, Temperature and Details, Time ad Location and services

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
+import TemperatueAndDetails from './components/TemperatueAndDetails';
+import Forecast from './components/Forecast';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons/>
       <Inputs/>
+
+      <TimeAndLocation/>
+      <TemperatueAndDetails/>
+
+      <Forecast title="hourly forecast"/>
+      <Forecast title="daily forecast"/>
     </div>
   );
 }
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -3,16 +3,29 @@ import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
 
 function Inputs() {
   return (
-    <div className='flex flex-row justify-center my-6 space-x-10'>
+    <div className='flex flex-row justify-center my-6 space-x-20'>
+
+        {/* Search */}
         <div className='flex flex-row w=3/4 items-center justify-center space-x-4'>
+
+            {/* Search Bar */}
             <input type="text" className='text-xl font-light p-2 w-full shadow-xl capitalize focus:outline-none placeholder:lowercase' placeholder='Search for city.....'/>
+
+            {/* Search Icon */}
             <UilSearch size={25} className='text-white cursor-pointer transition ease-out hover:scale-125'/>
+
+            {/* Search by location */}
             <UilLocationPoint size={25} className='text-white cursor-pointer transition ease-out hover:scale-125'/>
         </div>
 
+        {/* Temperature Conversion */}
         <div className='flex flex-row items-center justify-center w=1/4'>
+
+            {/* Celcius */}
             <button name='metric' className='text-xl text-white font-light'>°C</button>
             <p className='text-xl text-white mx-1'>|</p>
+
+            {/* Fahrenheit */}
             <button name='imperial' className='text-xl text-white font-light'>°F</button>
         </div>
     </div>
